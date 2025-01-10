9a785002fb8593ea6910a9890ee6884d4a388dee
Noah <noah.dangerfield@gmail.com>
2025-01-09 18:42:20 -0800

    Some updates

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import Optional, Annotated
+
 import typer
 from documentcloud import DocumentCloud
 from documentcloud.exceptions import (
@@ -10,20 +12,25 @@ from documentcloud.exceptions import (
 from rich import print
 from rich.table import Table
 
+app = typer.Typer()
 
-def search(query: str, result_count: int=10):
+@app.command()
+# Doesn't show 'INTEGER' in help instead shows '[RESULT_COUNT]' as type instead :/
+def search(query: str, result_count: Annotated[int, typer.Argument(min=1, max=25)]=10):
     client = DocumentCloud()
     doc_list = client.documents.search(query).results[:result_count]
     if not doc_list:  # List is empty (no results for query)
         print("[bold red]Your query returned no results!")
     else:
-        table = Table(title="Search Results")
+        table = Table(title="[red]Search Results")
         table.add_column("Contributor", justify="center", style="cyan")
         table.add_column("Title", justify="center", style="magenta")
-        table.add_column("Creation Date", justify="center", style="cyan")
+        table.add_column("Creation Date", justify="center", style="green")
         for doc in doc_list:
-            table.add_row(f"{doc.contributor}", f"[link={doc.canonical_url}]{doc.title}[/link]", f"{doc.created_at.strftime('%b %d %Y')}")
+            table.add_row(f"{doc.contributor}",
+                          f"[link={doc.canonical_url}]{doc.title}[/link]",
+                          f"{doc.created_at.strftime('%b %d %Y')}")
         print(table)
 
-if __name__ == "__main__": # testing
-    typer.run(search("Seattle", 3))
+if __name__ == "__main__":
+    app()
