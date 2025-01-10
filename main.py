080d893aeea35736dcd3d54ed94999bec32e5e67
Noah <noah.dangerfield@gmail.com>
2025-01-09 17:54:04 -0800

    Changing to Typer, search command initial implementation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import typer
 from datetime import datetime
 from documentcloud import DocumentCloud
 from documentcloud.exceptions import (
@@ -8,74 +9,24 @@ from documentcloud.exceptions import (
     DoesNotExistError,
     MultipleObjectsReturnedError
 )
+from rich import print
+from rich.table import Table
 
-def execute_search():
-    asking_limit = True
-    while asking_limit:
-        try:
-            limit = int(input("Enter the maximum amount of search results displayed (up to 25): "))
-            if 1 <= limit <= 25:
-                asking_limit = False
-        except ValueError:
-            pass
-    query = input("Type your search query: ").strip()
-    doc_list = client.documents.search(query).results[:limit]
-    if not doc_list: # List is empty (no results for query)
-        print("Your query returned no results.")
+
+def search(query: str, result_count: int=10):
+    client = DocumentCloud()
+    doc_list = client.documents.search(query).results[:result_count]
+    if not doc_list:  # List is empty (no results for query)
+        print("[bold red]Your query returned no results!")
     else:
-        for i, doc in enumerate(doc_list):
-            print(f"{i + 1}: \"{doc.title}\" - {doc.contributor} - {doc.created_at.strftime('%b %d %Y')}")
-        execute_inspect(doc_list, limit)
+        table = Table(title="Search Results")
+        table.add_column("Contributor", justify="center", style="cyan")
+        table.add_column("Title", justify="center", style="magenta")
+        table.add_column("Creation Date", justify="center", style="cyan")
+        for doc in doc_list:
+            table.add_row(f"{doc.contributor}", f"{doc.title}", f"{doc.created_at.strftime('%b %d %Y')}")
+        print(table)
+            # print(f"{i + 1}: \"{doc.title}\" - {doc.contributor} - {doc.created_at.strftime('%b %d %Y')}")
 
-def execute_inspect(doc_list, result_limit):
-    try:
-        selection_choice = int(input("Enter the number of the document you want to "
-                                     f"inspect (1-{result_limit}) Anything else to exit: "))
-    except ValueError:
-        sys.exit()
-    if 1 <= selection_choice <= 10:
-        doc = doc_list[selection_choice - 1]
-        print(f"{'Metadata fields':_^35}")
-        metadata_fields = ["id", "access", "canonical_url", "created_at", "title", "page_count"]
-        for field in metadata_fields:
-            attribute = getattr(doc, field)
-            if isinstance(attribute, datetime): # need to handle converting datetime object to string.
-                attribute = attribute.strftime('%b %d %Y')
-            print(f"{field:.<30}{attribute}")
-
-def execute_upload():
-    pass
-
-# --initial setup--
-setup_loop = True
-while setup_loop:
-    username_result = input("Please provide your DocumentCloud username, type 'G' to remain a guest: ")
-    if username_result == 'G':
-        client = DocumentCloud()
-        setup_loop = False
-    else:
-        password_result = input("Please provide your DocumentCloud password: ").strip()
-        try:
-            client = DocumentCloud(username_result, password_result)
-            setup_loop = False
-        except CredentialsFailedError: # Re-prompt for username and password
-            print("Invalid username and/or password\n--------------------------------")
-
-
-
-loop = True
-while loop:
-    asking_action = True
-    while asking_action:
-        try:
-            print("-------------------------------------------------------------")
-            action_choice = int(input("What would you like to do? Exit (0), Search (1), Upload (2): "))
-            match action_choice:
-                case 0:
-                    sys.exit()
-                case 1:
-                    execute_search()
-                case 2:
-                    execute_upload()
-        except ValueError:
-            pass
+if __name__ == "__main__":
+    typer.run(search("Los Angeles"))
